a35f3ef3a4bed078ff2b8c743af76a88ee319e29
Chamunorwa Mutezva <ckmutezva@gmail.com>
2019-08-12 22:04:03 +0200

    feat: email validation , picture image use

--- main.js
+++ main.js
@@ -1,4 +1,26 @@
-//Intersection Observer api, 
+//Intersection Observer api,
+let invalidClassName = 'invalid';
+let inputs = document.querySelectorAll('input');
+let msg = document.querySelector('.msg');
+console.log(inputs);
+
+inputs.forEach(function (input) {
+    // Add a css class on submit when the input is invalid.
+    input.addEventListener('invalid', function () {
+        input.classList.add(invalidClassName);
+        msg.innerHTML = "Provide a valid email";
+    })
+
+    // Remove the class when the input becomes valid.
+    // 'input' will fire each time the user types
+    input.addEventListener('input', function () {
+        if (input.validity.valid) {
+            input.classList.remove(invalidClassName);
+            msg.innerHTML = " ";
+        }
+    })
+})
+    
 const headerFeature = document.querySelectorAll(".styled");
 observer = new IntersectionObserver((entries) => {
     entries.forEach(entry => {
@@ -16,3 +38,4 @@ observer = new IntersectionObserver((entries) => {
 headerFeature.forEach(heads => {
     observer.observe(heads);
 });
+
